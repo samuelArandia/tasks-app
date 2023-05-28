442c9d1739e03a795a7127a970001bbe199cfe05
Samuel Arandia <samuelarandia@gmial.com>
2023-05-28 12:17:48 -0400

    init

---
+++ backend/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("./app"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_app["default"].listen(3333);
+console.log('Server listening on port 3333');
---
+++ backend/dist/routes/tasks.js
@@ -0,0 +1,73 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _express = require("express");
+var _tasks = require("../controllers/tasks");
+var router = (0, _express.Router)();
+
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoints
+ */
+
+/**
+ * @swagger
+ * /tasks:
+ * get:
+ *  summary: Get all tasks
+ *  tags: [Tasks]
+ */
+router.get('/tasks', _tasks.getTasks);
+
+/**
+ * @swagger
+ * /tasks/count:
+ * get:
+ *  summary: Get total tasks counter 
+ *  tags: [Tasks]
+ */
+router.get('/tasks/count', _tasks.getCount);
+
+/**
+ * @swagger
+ * /tasks:
+ * get:
+ *  summary: Get task by id
+ *  tags: [Tasks]
+ */
+router.get('/tasks/:id', _tasks.getTask);
+
+/**
+ * @swagger
+ * /tasks:
+ * post:
+ *  summary: save a new task
+ *  tags: [Tasks]
+ */
+router.post('/tasks', _tasks.saveTask);
+
+/**
+ * @swagger
+ * /tasks:
+ * delete:
+ *  summary: Delete a task by id
+ *  tags: [Tasks]
+ * 
+ */
+router["delete"]('/tasks/:id', _tasks.deleteTasks);
+
+/**
+ * @swagger
+ * /tasks:
+ * put:
+ *  summary: Update a task by id
+ * tags: [Tasks]
+ */
+router.put('/tasks/:id', _tasks.updateTask);
+var _default = router;
+exports["default"] = _default;
